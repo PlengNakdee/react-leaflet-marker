cce405aaf20c8cf95f01c2ffd31539b0bfb049ae
PlengNakdee <plengwn@gmail.com>
2023-03-03 04:37:08 +0000

    use leaflet-geoman

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import { Circle, FeatureGroup, MapContainer, TileLayer } from 'react-leaflet';
-import { EditControl } from 'react-leaflet-draw';
 import './App.css';
+import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
+import Geoman from "./geoman";
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 const latitude = 13.7554
 const longitude = 100.4932
@@ -34,60 +34,10 @@ function DisplayPosition({ map }: any) {
   )
 }
 
-const DrawTool = () => {
-  // const _onDrawStart = (e: any) => {
-	// 	console.log("_onDrawStart", e);
-	// };
-
-  const _onCreated = (e: { layerType: any; layer: any; }) => {
-		let type = e.layerType;
-		let layer = e.layer;
-    if(type == 'polyline') {
-      var coords = layer.getLatLngs();
-      var length = 0;
-      for (let i = 0; i < coords.length - 1; i++) {
-        length += coords[i].distanceTo(coords[i + 1]);
-      }
-      console.log('line length in meter', length.toFixed(2));
-    }
-		if (type === "marker") {
-			// Do marker specific actions
-			console.log("_onCreated: marker created", e);
-		} else {
-			console.log("_onCreated: something else created:", type, e);
-		}
-
-		console.log("Geojson", layer.toGeoJSON());
-		console.log("coords", layer.getLatLngs());
-		// Do whatever else you need to. (save to db; etc)
-
-		// this._onChange();
-  }
-
-  return (
-    <FeatureGroup>
-    <EditControl
-      position='topleft'
-      // onDrawStart={_onDrawStart}
-      onCreated={_onCreated}
-      //onEdited={this._onEditPath}
-      //onDeleted={this._onDeleted}
-      draw={{
-        rectangle: false
-      }}
-    />
-    {/* <Circle center={[51.51, -0.06]} radius={200} /> */}
-  </FeatureGroup>
-  )
-}
-
-
-
-
-
-function App() {
+export default function App() {
   const [map, setMap] = useState()
-  const displayMap = useMemo(
+
+    const displayMap = useMemo(
     () => (
       <div>
         <p>Hello</p>
@@ -95,20 +45,30 @@ function App() {
         center={[latitude, longitude]}
         zoom={zoom}
         scrollWheelZoom={false}
+        // @ts-ignore
         ref={setMap}
         >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <DrawTool/>
+      <LayersControl position="topleft">
+        <LayersControl.BaseLayer checked name="OpenStreetMap">
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </LayersControl.BaseLayer>
+        {/* <LayersControl.BaseLayer checked name="MapBox VectorGrid">
+          <VectorGrid />
+        </LayersControl.BaseLayer> */}
+        {/* <LayersControl.Overlay name="geoJSON">
+          <GeoJSON data={geoJSONData} />
+        </LayersControl.Overlay> */}
+      </LayersControl>
+      <Geoman />
       </MapContainer>
-      
       </div>
-
     ),
     [],
   )
+
   return (
     <div>
     {map ? <DisplayPosition map={map} /> : null}
@@ -116,5 +76,3 @@ function App() {
   </div>
   );
 }
-
-export default App;
